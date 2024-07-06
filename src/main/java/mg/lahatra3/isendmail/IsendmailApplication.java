a17edb071c8a4504003e5cdeaecdd156e07b4d1a
lahatra3 <lahatrap20.aps2a@gmail.com>
2024-07-06 21:04:20 +0300

    feat(isendmail): init project...

---
+++ src/main/java/mg/lahatra3/isendmail/IsendmailApplication.java
@@ -0,0 +1,13 @@
+package mg.lahatra3.isendmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsendmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsendmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/lahatra3/isendmail/IsendmailApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.lahatra3.isendmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsendmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
